54ef587244eac1f88a17789908314ad768382938
Ryan Zheng <nogrethumphrey@gmail.com>
2020-04-23 09:15:18 +0200

    added initial project

---
+++ SpringBootGraphqlStarter/src/main/java/com/qiusuo/SpringBootGraphqlStarter/SpringBootGraphqlStarterApplication.java
@@ -0,0 +1,13 @@
+package com.qiusuo.SpringBootGraphqlStarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootGraphqlStarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootGraphqlStarterApplication.class, args);
+	}
+
+}
---
+++ SpringBootGraphqlStarter/src/test/java/com/qiusuo/SpringBootGraphqlStarter/SpringBootGraphqlStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qiusuo.SpringBootGraphqlStarter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootGraphqlStarterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
